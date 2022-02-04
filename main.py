2f581122c1501db0c2608f80a113b4e5e8210009
thoth <thoth2357@gmail.com>
2022-02-04 12:53:56 +0100

    folder structure creation done

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 #importing modules
 import sys
 import os
+import subprocess
 
 
 #create folder
 folder_name = sys.argv[1]
-print('folder_name', folder_name)
+folder_Structure = ['public', 'src']
+if not os.path.exists(os.path.join(os.getcwd(), folder_name)):
+    print('Creating Directory structure')
+    for folder in folder_Structure:
+        os.makedirs(f'{os.path.join(os.getcwd, folder_name)}/{folder}')
+
+#install tailwind
+print('Installing TailWind')
+gi
+    
+
+
+
