c8659c30585ff1c9846b770cc91e90e458bb964d
thoth <thoth2357@gmail.com>
2022-02-04 13:03:20 +0100

    revamped code base to functions

--- main.py
+++ main.py
@@ -2,19 +2,39 @@
 import sys
 import os
 import subprocess
+import getopt
 
+from click import argument
+
+
+arguments = sys.argv[1:]
+  
+try:
+    opts, args = getopt.getopt(arguments, "n:l:", 
+                                ["project_name ="])
+    
+except:
+    print("Error In Flag argument passed")
+
+for opt, arg in opts:
+    if opt in ['-n', '--project_name']:
+        folder_name = arg
 
 #create folder
-folder_name = sys.argv[1]
-folder_Structure = ['public', 'src']
-if not os.path.exists(os.path.join(os.getcwd(), folder_name)):
-    print('Creating Directory structure')
-    for folder in folder_Structure:
-        os.makedirs(f'{os.path.join(os.getcwd, folder_name)}/{folder}')
+def create_folder():
+    'Creates folder structure'
+    folder_Structure = ['public', 'src']
+    if not os.path.exists(os.path.join(os.getcwd(), folder_name)):
+        print('Creating Directory structure')
+        for folder in folder_Structure:
+            os.makedirs(f'{os.path.join(os.getcwd, folder_name)}/{folder}')
 
+def install_tailwind():
+    'Installs tailwind and configures it'
 #install tailwind
 print('Installing TailWind')
-gi
+subprocess.
+
     
 
 
